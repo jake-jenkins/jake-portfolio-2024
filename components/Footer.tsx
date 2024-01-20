eda64b1919c9a0ee81eee1fa62ea778045debd83
jake <jake@jake1.net>
2024-01-20 19:49:52 +0000

    init

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import { P } from "./Typography";
+
+export default function Footer() {
+    return (
+        <footer className="p-4 flex items-center container mx-auto">
+            <P>© Jake Jenkins and respective owners 2003 - 2023. All rights reserved.</P>
+        </footer>
+    )
+}
