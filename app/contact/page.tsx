24d42fb341a39b9af6b9d00f1f22c1bf3c84f4e0
Jake Jenkins <jake@jake1.net>
2024-01-24 23:28:47 +0000

    weeny

---
+++ app/contact/page.tsx
@@ -0,0 +1,17 @@
+import { H1, P } from "@/components/Typography";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title:
+    "Contact Jake Jenkins, Web Developer and Experience Designer - Jake1.net",
+  description: "Web and mobile applications built by Jake Jenkins",
+};
+
+export default function ContactPage() {
+  return (
+    <>
+      <H1>Contact</H1>
+      <P>Page coming soon.</P>
+    </>
+  );
+}
